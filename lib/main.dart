64790ddeb09c567994894d0e889038fb92f4d8bd
tranquanghuy123 <130018198+tranquanghuy123@users.noreply.github.com>
2023-06-18 13:57:13 +0700

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,136 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+ runApp(MaterialApp(
+   home: SafeArea(
+     child: Scaffold(
+       body: MyApp(),
+     ),
+   ),
+ ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData( 
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Container(
+      child: Container(
+      child: Column(
+        children: [
+          Container(
+            child: const Image(
+              image: NetworkImage('https://github.com/flutter/website/blob/main/examples/layout/lakes/step6/images/lake.jpg?raw=true'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Container(
+            height: 65,
+            width: 400,
+            padding: EdgeInsets.fromLTRB(15,5,15,5),
+            //decoration: BoxDecoration(
+              //border: Border.all(color: Colors.black, width: 3)
+            //),
+            margin: const EdgeInsets.fromLTRB(10,20,10,5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Oeschinen Lake Campground',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 14, color: Colors.black, fontWeight: FontWeight.w600
+                      ),
+                    ),
+
+                    Text('Kandersteg, Switzerland',
+                      style: TextStyle(
+                        fontSize: 13, color: Colors.grey
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.star,
+                      color: Colors.red,),
+                    Text('41'),
+                  ],
+                )
+
+              ],
+            )
+          ),
+            Container(
+            height: 75,
+            width: 300,
+            padding: EdgeInsets.fromLTRB(20,5,20,5),
+            //decoration: BoxDecoration(
+            //border: Border.all(color: Colors.black, width: 3)
+            //),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.call,color: Colors.lightBlue,size: 25),
+                      Text('CALL',
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: Colors.lightBlue
+                      )),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.near_me,color: Colors.lightBlue,size: 25),
+                      Text('ROUTE',
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.lightBlue
+                          )),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.share ,color: Colors.lightBlue,size: 25),
+                      Text('SHARE',
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.lightBlue
+                          )),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ],
-        ),
+                Container(
+                height: 160,
+                width: 350,
+                  padding: EdgeInsets.fromLTRB(5,10,5,10),
+                  margin: const EdgeInsets.fromLTRB(10,5,10,5),
+                //decoration: BoxDecoration(
+                //border: Border.all(color: Colors.black, width: 3)
+                  //),
+                  child: Text('Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+                      'Alps. Situated 1,578 meters above sea level, it is one of the '
+                      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+                      'half-hour walk through pastures and pine forest, leads you to the '
+                      'lake, which warms to 20 degrees Celsius in the summer. Activities '
+                      'enjoyed here include rowing, and riding the summer toboggan run.'
+                      ,
+                        textAlign: TextAlign.left),
+                )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
+
     );
   }
 }
